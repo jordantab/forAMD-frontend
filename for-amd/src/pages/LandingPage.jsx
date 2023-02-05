c8e8025b56b58f1cbc7e1e4bf17c1ef53defe613
Jordan Tab <taboglut@bu.edu>
2023-02-05 16:54:51 -0500

    Trying different videos

--- for-amd/src/pages/LandingPage.jsx
+++ for-amd/src/pages/LandingPage.jsx
@@ -1,13 +1,16 @@
 import NavBar from "../components/NavBar"
 import './LandingPage.css'
-import myVideo from "./1.MOV";
+import myVideo from "../videos/1.MOV";
+import myVideo2 from "../videos/nye1.MOV";
+import myVideo3 from "../videos/2.MOV";
+import myVideo4 from "../videos/facemasks.MOV";
 
 
 function LandingPage() {
     return (
         <div className="Landing">
             <video loop autoPlay muted>
-                <source src={myVideo} type="video/mp4"/>
+                <source src={myVideo2} type="video/mp4"/>
             </video>
             <h2>a trip down memory lane</h2>
             <div className="navbar">
