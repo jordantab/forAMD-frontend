bf5e7d028b331e09d6bf24ee5fbcab2646e80934
Jordan Tab <taboglut@bu.edu>
2023-02-05 16:59:23 -0500

    Trying different videos

--- for-amd/src/pages/LandingPage.jsx
+++ for-amd/src/pages/LandingPage.jsx
@@ -10,7 +10,7 @@ function LandingPage() {
     return (
         <div className="Landing">
             <video loop autoPlay muted>
-                <source src={myVideo2} type="video/mp4"/>
+                <source src={myVideo} type="video/mp4"/>
             </video>
             <h2>a trip down memory lane</h2>
             <div className="navbar">
